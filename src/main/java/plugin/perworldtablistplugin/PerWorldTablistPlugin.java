2b50c2c8ce0d759ba2b88d6cbbc0a3a21b30beb1
jairusu <jairusudanieru@gmail.com>
2023-07-01 17:05:36 +0800

    perworldtablist

--- src/main/java/plugin/perworldtablistplugin/PerWorldTablistPlugin.java
+++ src/main/java/plugin/perworldtablistplugin/PerWorldTablistPlugin.java
@@ -11,15 +11,22 @@ import plugin.perworldtablistplugin.Events.PlayerQuit;
 
 public final class PerWorldTablistPlugin extends JavaPlugin {
 
+    public void checkAuthMe() {
+        Plugin authMe = Bukkit.getServer().getPluginManager().getPlugin("AuthMe");
+        if (authMe != null) {
+            Bukkit.getPluginManager().registerEvents(new AuthMeEvent(this),this);
+        } else {
+            Bukkit.getPluginManager().registerEvents(new PlayerJoin(this),this);
+        }
+    }
+
     @Override
     public void onEnable() {
         saveDefaultConfig();
+        checkAuthMe();
         Bukkit.getPluginCommand("pwtp").setExecutor(new Pwtp(this));
         Bukkit.getPluginManager().registerEvents(new ChangeWorld(this),this);
-        Bukkit.getPluginManager().registerEvents(new PlayerJoin(this),this);
         Bukkit.getPluginManager().registerEvents(new PlayerQuit(this),this);
-        Plugin authMe = Bukkit.getServer().getPluginManager().getPlugin("AuthMe");
-        if (authMe != null) Bukkit.getPluginManager().registerEvents(new AuthMeEvent(this),this);
         Bukkit.getLogger().info("[PerWorldTablistPlugin] Plugin Successfully Enabled!");
     }
 
